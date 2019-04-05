b5e2d0b34336d08303209d80e4c1631bbe59beb0
Richard Borge <Richard.Borge@nav.no>
2019-04-05 20:19:46 +0200

    Setter noe så appen kan starte

--- src/main/kotlin/no/nav/modiapersonoversikt/storage/S3StorageProvider.kt
+++ src/main/kotlin/no/nav/modiapersonoversikt/storage/S3StorageProvider.kt
@@ -2,7 +2,7 @@ package no.nav.modiapersonoversikt.storage
 
 class S3StorageProvider : StorageProvider {
     override fun loadData(): MutableMap<String, MutableMap<String, Any>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return mutableMapOf()
     }
 
     override fun storeData(data: MutableMap<String, MutableMap<String, Any>>) {
