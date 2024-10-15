3de49c8cfadac86978f7e0bf2e2c7f6191bcf4d0
Ludvig Hozman <ludvig123@gmail.com>
2024-10-15 13:35:44 +0200

    Bruk java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,12 +77,12 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+    kotlinOptions.jvmTarget = "21"
     kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
 
